9a0fba6d34fdc1a78f951f5153074a57acc8348e
kaushal <kaushal@nimidev.com>
2023-08-27 20:35:29 +0530

    inital commit

---
+++ src/main/java/com/pronto/test/ProntoTestApplication.java
@@ -0,0 +1,13 @@
+package com.pronto.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProntoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pronto/test/ProntoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pronto.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
